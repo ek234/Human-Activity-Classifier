792902be4a0066f4c34d7ea40918582b83b1aace
ek234 <71319388+ek234@users.noreply.github.com>
2023-05-07 03:23:32 +0530

    updated dt to return more metric

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -2,8 +2,7 @@ import sys
 import pickle
 import numpy as np
 from time import time
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
+import sklearn.metrics
 from sklearn.model_selection import train_test_split
 
 from sklearn.tree import DecisionTreeClassifier
@@ -28,13 +27,20 @@ def create_model (depth, isred):
 
     pickle.dump(dt, open(f'../models/dt_{depth}_{isred}.pkl', 'wb'))
     y_pred_val = dt.predict(X_val)
-    acc_val = accuracy_score(y_val, y_pred_val)
+    acc_val = sklearn.metrics.accuracy_score(y_val, y_pred_val)
     # print(f"Accuracy wrt validation set for full data with C={c}: {acc_val}")
 
     y_pred_test = dt.predict(X_test)
-    acc_test = accuracy_score(y_test, y_pred_test)
+    acc_test = sklearn.metrics.accuracy_score(y_test, y_pred_test)
     # print(f"Accuracy wrt test set for reduced data with C={c}: {acc_test}")
-    return train_dur, acc_val, acc_test
+    confusion_matrix_test = sklearn.metrics.confusion_matrix(y_test, y_pred_test)
+    class_rep = sklearn.metrics.classification_report(y_test, y_pred_test)
+
+    y_pred_train = dt.predict(X_train)
+    acc_train = sklearn.metrics.accuracy_score(y_train, y_pred_train)
+    # print(f"Accuracy wrt test set for reduced data with C={c}: {acc_test}")
+    
+    return train_dur, acc_val, acc_test, acc_train, confusion_matrix_test, class_rep
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -44,8 +50,11 @@ if __name__ == "__main__":
     use_reduced = sys.argv[2] == "red"
     print(f"starting dt exec->  depth: {depth}, using reduced set: {use_reduced}")
 
-    train_time, acc_v, acc_t = create_model(depth, use_reduced)
+    train_time, acc_v, acc_t, acc_tr, confusion_matrix_test, class_rep = create_model(depth, use_reduced)
     with open(f"../models/dt_metrics_{sys.argv[2]}_{sys.argv[1]}.txt", 'w') as file:
         file.write(f"train time: {train_time}\n")
         file.write(f"accuracy v: {acc_v*100:.3f}%\n")
-        file.write(f"accuracy t: {acc_t*100:.3f}%\n")
+        file.write(f"accuracy te: {acc_t*100:.3f}%\n")
+        file.write(f"accuracy tr: {acc_tr*100:.3f}%\n")
+        file.write(f"confusion matrix:\n{confusion_matrix_test}\n")
+        file.write(f"classification report:\n{class_rep}\n")
